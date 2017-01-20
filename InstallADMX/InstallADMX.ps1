84f38e93ccf2a2c4fdcf317ed513089162625770
Andy Taylor <andy0taylor@gmail.com>
2017-01-20 14:34:21 +0000

    Added Server 2016 Templates

--- InstallADMX/InstallADMX.ps1
+++ InstallADMX/InstallADMX.ps1
@@ -17,9 +17,9 @@ Write-Host "Downloading latest Server 2012R2 / Windows 8.1 Templates"
 Write-Host ""
 Download-File "https://download.microsoft.com/download/7/F/5/7F5E839D-8254-46D6-B9E0-F1B555AFA48B/Windows8.1-Server2012R2ADMX-RTM.msi"
 Write-Host ""
-Write-Host "Downloading latest Windows 10 Templates"
+Write-Host "Downloading latest Server 2016 Templates"
 Write-Host ""
-Download-File "https://download.microsoft.com/download/2/E/3/2E3A1E42-8F50-4396-9E7E-76209EA4F429/Windows10_Version_1511_ADMX.msi"
+Download-File "https://download.microsoft.com/download/0/C/0/0C098953-38C6-4DF7-A2B6-DE10A57A1C55/Windows%2010%20and%20Windows%20Server%202016%20ADMX.msi"
 Write-Host ""
 Write-Host "Extracting MSI's."
 Write-Host ""
@@ -33,7 +33,7 @@ Extract-MSI $name $psscriptroot\$folder
 Write-Host "Moving Templates"
 #Get-ChildItem $psscriptroot\$folder\PolicyDefinitions -Recurse |Move-Item -Destination c:\windows\sysvol\domain\policies\PolicyDefinitions -Force
 
-$name="Windows10_Version_1511_ADMX.msi"
+$name="Windows%2010%20and%20Windows%20Server%202016%20ADMX.msi"
 $folder=$name.Trim(".msi")
 Write-Host "Extracting $name"
 Extract-MSI $name $psscriptroot\$folder
