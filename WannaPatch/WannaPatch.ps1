b584bb8c5fd7cb33785a3e70b1e29855954eed8a
Andy Taylor <andy@home.404.me.uk>
2017-05-15 16:51:35 +0100

    Fixed dual patch logic

--- WannaPatch/WannaPatch.ps1
+++ WannaPatch/WannaPatch.ps1
@@ -5,33 +5,39 @@ $OS.Caption
 
 
 function CheckForPatch($KB,$KB2)
+
 {
-if(Get-HotFix $KB -ErrorAction SilentlyContinue -or Get-HotFix $KB2 -ErrorAction SilentlyContinue)
+$KB
+$KB2
+$Monthly=Get-HotFix $KB -ErrorAction SilentlyContinue
+$Security=Get-HotFix $KB2 -ErrorAction SilentlyContinue
+
+if($Monthly.hotfixid -eq $KB -or $Security.hotfixid -eq $KB2)
             {
                 Write-Host "Patch Installed"
             }
         Else
             {
                 Write-Host "Patch NOT Installed"
-                Start-Process "https://www.catalog.update.microsoft.com/Search.aspx?q=$KB"
+                Start-Process "https://www.catalog.update.microsoft.com/Search.aspx?q=$KB2"
             }    
 }
 ##  Check for Patch 
 
 if($OS.Caption -eq "Microsoft Windows Server 2008 R2 Standard")
     {
-        CheckForPatch("KB4012212","KB4012215")
+        CheckForPatch -KB "KB4012212" -KB2 "KB4012215"
     }
 if($OS.Caption -eq "Microsoft Windows Server 2012 Standard")
     {
-        CheckForPatch("KB4012214","KB4012217")
+        CheckForPatch -KB "KB4012214" -KB2 "KB4012217"
     }
 if($OS.Caption -eq "Microsoft Windows Server 2012 R2 Standard")
     {
-        CheckForPatch("KB4012213","KB4012216")
+        CheckForPatch -KB "KB4012213" -KB2 "KB4012216"
     }
 if($OS.Caption -eq "Microsoft Windows Server 2016 Standard")
     {
-        CheckForPatch("KB4013429,KB4013429")
+        CheckForPatch -KB "KB4013429" -KB2 "KB4013429"
     }
 Sleep 60
