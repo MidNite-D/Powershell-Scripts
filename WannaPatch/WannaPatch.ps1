e2fcba67d73b9a02d14b70bdf3bee153b72392f0
Andy Taylor <andy@home.404.me.uk>
2017-05-16 15:11:59 +0100

    Added Check for Previous Rollups

--- WannaPatch/WannaPatch.ps1
+++ WannaPatch/WannaPatch.ps1
@@ -4,15 +4,16 @@ $OS = Get-WMIObject Win32_OperatingSystem
 $OS.Caption
 
 
-function CheckForPatch($KB,$KB2)
+function CheckForPatch($KB,$KB2,$KB3)
 
 {
 $KB
 $KB2
 $Monthly=Get-HotFix $KB -ErrorAction SilentlyContinue
 $Security=Get-HotFix $KB2 -ErrorAction SilentlyContinue
+$Previous=Get-HotFix $KB3 -ErrorAction SilentlyContinue
 
-if($Monthly.hotfixid -eq $KB -or $Security.hotfixid -eq $KB2)
+if($Monthly.hotfixid -eq $KB -or $Security.hotfixid -eq $KB2 -or $Previous.hotfixid -eq $KB3)
             {
                 Write-Host "Patch Installed"
             }
@@ -26,26 +27,26 @@ if($Monthly.hotfixid -eq $KB -or $Security.hotfixid -eq $KB2)
 
 if($OS.Caption -like "*2008 Standard*")
     {
-        CheckForPatch -KB "KB4012598" -KB2 "KB4012598"
+        CheckForPatch -KB "KB4012598" -KB2 "KB4012598" -KB3 "KB4012598"
     }
 if($OS.Caption -like "*2008 Enterprise*")
     {
-        CheckForPatch -KB "KB4012598" -KB2 "KB4012598"
+        CheckForPatch -KB "KB4012598" -KB2 "KB4012598" -KB3 "KB4012598"
     }
 if($OS.Caption -like "*Server 2008 R2*")
     {
-        CheckForPatch -KB "KB4012212" -KB2 "KB4012215"
+        CheckForPatch -KB "KB4012212" -KB2 "KB4012215" -KB3 "KB3212646"
     }
 if($OS.Caption -eq "Microsoft Windows Server 2012 Standard")
     {
-        CheckForPatch -KB "KB4012214" -KB2 "KB4012217"
+        CheckForPatch -KB "KB4012214" -KB2 "KB4012217" -KB3 "KB3205409"
     }
 if($OS.Caption -like "*Windows Server 2012 R2*")
     {
-        CheckForPatch -KB "KB4012213" -KB2 "KB4012216"
+        CheckForPatch -KB "KB4012213" -KB2 "KB4012216" -KB3 "KB3205401"
     }
 if($OS.Caption -like "*Windows Server 2016*")
     {
-        CheckForPatch -KB "KB4013429" -KB2 "KB4013429"
+        CheckForPatch -KB "KB4013429" -KB2 "KB4013429" -KB3 "KB3213986"
     }
 Sleep 60
