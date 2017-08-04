596e102dc970f6d4829a0aa7cfbe807333cb8c20
Andy Taylor <andy@home.404.me.uk>
2017-08-04 09:22:00 +0100

    updated

--- InstallADMX/InstallADMX.ps1
+++ InstallADMX/InstallADMX.ps1
@@ -1,7 +1,7 @@
-cls
+clear-host
 function Download-File ($file)
 {
-Invoke-Expression ".\curl.exe --progress-bar -O $file"
+Invoke-Expression "$psscriptroot\curl.exe --progress-bar -O $file"
 }
 
 Write-Host "Downloading Google Chrome Templates"
@@ -13,27 +13,27 @@ Write-Host ""
 Download-File "https://download.microsoft.com/download/0/C/0/0C098953-38C6-4DF7-A2B6-DE10A57A1C55/Windows%2010%20and%20Windows%20Server%202016%20ADMX.msi"
 Write-Host ""
 Write-Host "Downloading Office 2016 Templates."
-Download-File "https://download.microsoft.com/download/2/E/E/2EEEC938-C014-419D-BB4B-D184871450F1/admintemplates_x64_4468-1000_en-us.exe"
+Download-File "https://download.microsoft.com/download/2/E/E/2EEEC938-C014-419D-BB4B-D184871450F1/admintemplates_x64_4549-1000_en-us.exe"
+
 Write-Host "Extracting"
 Write-Host ""
 
 $name="Windows%2010%20and%20Windows%20Server%202016%20ADMX.msi"
 $folder=$name.Trim(".msi")
 Write-Host "Extracting $name"
-msiexec /a $name /qn TARGETDIR=$PSScriptRoot\Server |Out-Null
+start-process -filepath "C:\windows\System32\msiexec" -ArgumentList "/a $psscriptroot\$name /qn TARGETDIR=$PSScriptRoot\Server" -NoNewWindow -RedirectStandardOutput
 
 $name="admintemplates_x64_4468-1000_en-us.exe"
 $folder=$name.Trim(".exe")
 Write-Host "Extracting $name"
-.\admintemplates_x64_4468-1000_en-us.exe /quiet /passive /norestart /extract:$PSScriptRoot\Office
+start-process -FilePath "$psscriptroot\admintemplates_x64_4549-1000_en-us.exe" -ArgumentList "/quiet /passive /norestart /extract:$PSScriptRoot\Office"
 
-#$name="Windows%2010%20and%20Windows%20Server%202016%20ADMX.msi"
-#$folder=$name.Trim(".msi")
-#Write-Host "Extracting $name"
-#msiexec /a $name /qn TARGETDIR=$PSScriptRoot\$folder |Out-Null
+Expand-Archive -Path $PSScriptRoot\policy_templates.zip -DestinationPath $PSScriptRoot\chrome -ErrorAction SilentlyContinue
+
 
 Write-Host "Moving Templates"
 #Get-ChildItem $psscriptroot\$folder\PolicyDefinitions -Recurse |Move-Item -Destination c:\windows\sysvol\domain\policies\PolicyDefinitions -Force
+start-sleep 60
 
 
 
