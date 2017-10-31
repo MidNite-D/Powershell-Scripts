5b42c6238926fbc1c5e9c817db18a310fa2483af
Andy Taylor <andy@home.404.me.uk>
2017-10-31 09:31:55 +0000

    Removed Server build tools

--- RestoreGpo/RestoreGPOs.ps1
+++
@@ -1,9 +0,0 @@
-﻿$BackupPath = Read-Host "Location of backups ?"
-$backups=Get-ChildItem $BackupPath
-foreach($backup in $backups){
-$backupname=$backup.fullname+"\gpreport.xml"
-$Data = [Xml] (Get-Content "$backupname")
-New-GPO -Name $data.GPO.Name
-Import-GPO -BackupId $backup.Name -Path $Backuppath -TargetName $data.GPO.Name
-}
-
